f66812707a824153c6206ee9ce6a2f2518f1d8ca
florianCombarel <florian.combarel@orange.fr>
2017-12-06 09:29:01 +0100

    Update JaugeNaturelTest.java

--- JaugeNaturelTest.java
+++ JaugeNaturelTest.java
@@ -10,7 +10,7 @@ public class JaugeNaturelTest {
 
 	@Before
 	public void setUp() throws Exception {
-		jauge = new JaugeNaturel(100, 345, 67899);
+		jauge = new JaugeNaturel(100, 345, 200);
 	}
 
 	@After
@@ -72,16 +72,44 @@ public class JaugeNaturelTest {
 		assertTrue(!jauge.estBleu()&&jauge.estVert()&&!jauge.estRouge());
 	}
 	
-	@Test
+	/*@Test
 	public void testLimiteVigieMaxInferieurVigieMin() {
 		jauge = new JaugeNaturel(100, 98, 99);
 		assertFalse("jauge incoherente",jauge.getMax()<jauge.getValeur() && jauge.getValeur()<jauge.getMin());
-	}
+	}*/
 	
-	@Test
+	/*@Test
 	public void testMaxEgalMin() {
 		jauge = new JaugeNaturel(100, 100, 100);
 		assertFalse("jauge inutile, vitesse fixer à un seul rang donc improbable",jauge.getMax()<=jauge.getMin());
+	}*/
+	
+	/*
+	 * Il faut mettre deux blocs try catch dans le test ci dessous pour pouvoir connaitre quelle instanciation
+	 * de JaugeNaturel a renvoyé ou non une exception.
+	 * Si aucune exception n'est levée le test s'arrête lors de la vérification de la première variable,
+	 * il faut donc trouver un autre moyen de renvoyer les exceptions. Ce qui va être abordé dans le 3.
+	 */
+	@Test//(expected=IllegalArgumentException.class)
+	public void testCreationNonValide(){
+		try{
+			JaugeNaturel inverse = new JaugeNaturel(78, 13, 0);
+			fail("Exception not thrown on inverse");
+		}catch(IllegalArgumentException e1){
+			try{
+				JaugeNaturel egale = new JaugeNaturel(-45, -45, -45);
+				fail("Exception not thrown on egale");
+			}catch(IllegalArgumentException e2){
+			}
+		}
+	}
+	
+	@Test
+	public void testExceptionControlee() /*throws ClassNotFoundException()*/{
+		try{
+			throw new ClassNotFoundException("Attention");
+		}catch(ClassNotFoundException e){
+		}
 	}
 	
 	@Test
